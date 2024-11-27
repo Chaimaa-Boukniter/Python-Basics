138191bceedfc42bcb075b55704018b160757159
Chaimaa-Boukniter <chaimaaboukniter2906@gmail.com>
2024-11-27 19:18:01 +0100

    third commit

---
+++ python-fund/Tr.py
@@ -0,0 +1,6 @@
+
+
+for i in range (0,11):
+    for j in range (0,i):
+        print(f"*",end=" ")
+    print(f"\n")
---
+++ python-fund/while.py
@@ -0,0 +1,6 @@
+x = 1
+y = 20
+while(x != y):
+    print(f"x : {x} && y : {y}")
+    x += 1
+print(f"x : {x} && y : {y}")
